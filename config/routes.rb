ce4839ca808820473c36beff4bd2e8fe16f47f28
Quin <leongtsuquin@gmail.com>
2016-09-28 15:14:25 +0800

    added login, logout & register links

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
-  devise_for :players, 
+  root "home#index"
+  devise_for :players,
     controllers: {
       sessions: 'devise/sessions'
     },
