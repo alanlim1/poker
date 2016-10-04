cfb1762dca1d94968429f705d0b5d81682d93155
Adrian Yem <adrianyem91@outlook.com>
2016-10-04 16:36:11 +0800

    update hole

--- app/jobs/player_broadcast_job.rb
+++ app/jobs/player_broadcast_job.rb
@@ -1,8 +1,8 @@
 class PlayerBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(message)
-    ActionCable.server.broadcast 'player_channel', message
+  def perform(player_id, message)
+    ActionCable.server.broadcast "player_channel_#{player_id}", message
   end
 
 end
